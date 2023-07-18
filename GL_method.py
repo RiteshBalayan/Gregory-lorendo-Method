899932b133f9929f4f3d8b0bfe889fc08400675b
riteshbalayan <121563087+RiteshBalayan@users.noreply.github.com>
2023-07-18 14:27:16 +0100

    17th_july_change

--- GL_method.py
+++ GL_method.py
@@ -2,79 +2,87 @@ import numpy as np
 from scipy.integrate import quad, dblquad
 from scipy.special import erfc
 import matplotlib.pyplot as plt
-import time
-import sqlite3
 from GL_functions import GL_functions
 
 
-class GL_sample:
-    def __init__(self, gl_calculator):
-        self.gl_calculator = gl_calculator
-        self.w = np.random.uniform(self.gl_calculator.priors['w_min'], self.gl_calculator.priors['w_max'])
-        self.phi = np.random.uniform(0, 2*np.pi)
-        t_min = np.min(self.gl_calculator.data[:, 0])
-        t_max = np.max(self.gl_calculator.data[:, 0])
-        self.t = np.linspace(t_min, t_max, 500)
-        self._plot()
-
-    def _j(self, t):
-        return np.floor(1 + ((self.gl_calculator.m) * (np.mod((self.w)*t+self.phi,2*np.pi)) / (2*np.pi)))
-
-    def _r(self, t, *r_values):
-        if len(r_values) != self.gl_calculator.m:
-            raise ValueError(f"Expected {self.gl_calculator.m} r_values, got {len(r_values)}")
-        index = int(self._j(t)) - 1  # Subtract 1 to match Python's 0-indexing
-        return r_values[index]
-
-    def _plot(self):
-        r_values = np.random.randint(self.gl_calculator.rmin, self.gl_calculator.rmax, size=self.gl_calculator.m)
-        fig = plt.figure(figsize=(12, 6))
-        plt.plot(self.t, [self._r(time, *r_values) for time in self.t])
-        plt.xlabel('time')
-        plt.ylabel('flux')
-        plt.title('Function Plot')
-        plt.show()
-
-
-
-    
 
 class GL_calculator(GL_functions):
+    '''
+    This Class will utilise functions defined in GL_function class and have two type of method.
+    1) Performing Integrals
+    2) Ploting Graphs
+
+    INtegrals are necessarly calculation for bayesian inference in marginalisation step.
+    > If integral is over all the parameters we get probability distribution of model
+    > If integral is over all parameters except one. The result will give probability distribution of left out variable.
+
+    fill folling dictinary to instantiate class
+
+    Priors = {'bins': , 'rmin': , 'rmax': , 'w_min': , 'w_max': , 'w_resolution': }
+
+
+    Make sure data is numpy array with 3 columns 
+
+    '''
     def __init__(self, data, priors):
         self.data = data
         self.priors = priors
-        self.phi_limits = [1, 2 * np.pi]
+        self.phi_limits = [0, 2 * np.pi]
         self.w_values = np.linspace(self.priors['w_min'], self.priors['w_max'], self.priors['w_resolution'])
         self.m = self.priors['bins']
         self.rmin = self.priors['rmin']
         self.rmax = self.priors['rmax']
-        self.m_values = np.linspace(2, self.m, self.m - 1).astype(int)
+        self.b_values = np.linspace(2, self.m, self.m - 1).astype(int)
+        self.time = np.linspace(0, self.data[-1,0], 1000)#high resolution time for ploting only
         self.res = []
-        self.prob_data = []
-        self.normalised_prob = []
-        self.normalised_Pd_m = []
+        self.prob_m = []
+        self.prob_w = []
+        #data and priors are dictionary specified when class is instantiated
+        # m is max bins ,w_min/ w_max, r_min, r_max are specified in priors
+        #W_values are discrete values of w where calculation is performed
+        #b_values are diffrent model with number of bins where calculation is performed
+        # Last three are class variable, which will be calculated when class methods are performed on data
        
 
     def Pw_dm(self):
-        
-        self.res = [[quad(lambda phi: self.prob_w(w, phi), 
-                          self.phi_limits[0], self.phi_limits[1], epsabs=1.0e-1)[0] 
-                    for w in self.w_values] for m in self.m_values]
+        # Integral over phi, will give normalised probability of frequency
+        # Performed over all w_values and b_values
 
-        
+        self.res = [
+            [
+                quad(
+                    lambda phi: self.liklihood(w, phi, b)[0], 
+                    self.phi_limits[0], self.phi_limits[1], 
+                    epsabs=1.0e-1
+                )[0]  for w in self.w_values    
+            ] for b in self.b_values
+        ]
+        # notice w is not integrated
+
+
+
+
     def Pd_m(self):
-        self.prob_data = []
-        for mv in self.m_values:
-            self.m = mv
-            self.prob_data.append(dblquad(
-                lambda w, phi: self.prob_w(w, phi),
-                self.phi_limits[0], self.phi_limits[1],
-                lambda phi: self.priors['w_min'],
-                lambda phi: self.priors['w_max'],
-                epsabs=1.0e-2
-            )[0])
+        #Integral over all parameters values w, phi, Gives likelihood of model
+        #Performed for all the GL models with diffrent number of bins
+                
+        self.prob_m = [
+            (
+                dblquad(
+                    lambda w, phi : self.liklihood(w, phi, b)[1], 
+                    self.phi_limits[0], self.phi_limits[1], 
+                    lambda w: self.priors['w_min'],
+                    lambda w: self.priors['w_max'],
+                    epsabs=1.0e-2
+                )[0]
+            ) for b in self.b_values
+        ]
+
+
+
     
     def plot_data(self):
+        #Ploting the raw data, used for calculation
               
         fig = plt.figure(figsize=(12, 6))  # create a figure
         plt.scatter(self.data[:, 0], self.data[:, 1], color = 'red')
@@ -84,49 +92,89 @@ class GL_calculator(GL_functions):
         plt.grid(True)
         
         plt.show()
-           
+
+    def compute_fft(self):
+        dt = float(self.data[1:2,0]) - float(self.data[0:1,0])
+        N = len(self.data[:,0])
+
+        #Compute FFT
+        F = np.fft.fft(self.data[:,1])
+        F = np.abs(F[:N//2])  # take only the positive-frequency terms
+        freq = np.fft.fftfreq(N, dt)[:N//2]  # compute the frequencies
+        
+        #Compute power spectral density
+        power = np.abs(F)**2
+
+        return freq, power
+
+    def plot_FFT(self):
+        freq, power = self.compute_fft()
+
+        #plottting
+        plt.figure(figsize=(12,6))
+        plt.title('Frequency Power Spectrum')
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Power')
+
+        #only plot the frequencies in range(w_min, w_max)
+        mask = (freq >= self.priors['w_min']) & (freq <= self.priors['w_max'])
+        plt.plot(freq[mask], power[mask])
+        plt.show()
+
+    def _r(self, t, *r_values, w, phi, b):
+
+        index = self.j(t,w, phi, b) - 1  # Subtract 1 to match Python's 0-indexing
+        return r_values[index]   
+
+    def sample(self):
+
+        w = np.random.uniform(self.priors['w_min'], self.priors['w_max'])
+        phi = np.random.uniform(0, 2*np.pi)
+        b = np.random.randint(2 , self.m)
+
+        r_values = np.random.uniform(self.rmin, self.rmax, size = b)
+
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(self.time, [self._r(time, *r_values, w = w , phi = phi, b = b) for time in self.time])
+        plt.xlabel('time')
+        plt.ylabel('flux')
+        plt.title('Function Plot')
+        plt.show()
 
     def plot_Pw_m(self,specified_bin):
+        #Ploting probability distribution of frequency(Unormalised) for a specific model. Argument take number of bins of model.
+        #Cal be executed right after integral pw_dm, make sure calculation is done before executing
               
         fig = plt.figure(figsize=(12, 6))  # create a figure
-        plt.plot(self.w_values, self.res[specified_bin], 'o-')
+        plt.plot(self.w_values, (self.res[specified_bin-2]), 'o-')
         plt.xlabel('w')
-        plt.ylabel('Normalised Probability')
+        plt.ylabel('Unnormalised Probability')
         plt.show()
         
         
     def plot_Pd_m(self):
-        
-        max_Pd_m = max(self.prob_data)
-        
-        self.normalised_Pd_m = [x / max_Pd_m for x in self.prob_data]
-        
+        #Plot probability denstion of all GL periodic model
+
         fig = plt.figure(figsize=(12, 6))  # create a figure
-        
-        plt.plot(self.m_values, self.normalised_Pd_m, 'o-')
-        
+        plt.plot(self.b_values, self.prob_m, 'o-')
         plt.xlabel('Bins')
-        plt.ylabel('Normalised Probability')
-        
+        plt.ylabel('Unnormalised Probability')
         plt.show()
            
 
     def plot_Pw(self):
-        hio = [[self.prob_data[m-2] * x for x in res_row] for m, res_row in zip(self.m_values, self.res)]
-        pio = np.array(hio)
-        kio = sum(pio[i-2] for i in self.m_values)
-        nio = [x/sum(self.prob_data) for x in kio]
-        #Normalize Probability to max
-        max_prob = max(nio) 
-        self.normalised_prob = [x / max_prob for x in nio]
+        # plot probability density of frequency after averaging over all the models 
+
+        pio = np.array([[self.prob_m[b-2] * x for x in res_row] for b, res_row in zip(self.b_values, self.res)])  
+        #pio = np.array(hio)
+        kio = sum(pio[i-2] for i in self.b_values)
+        nio = [x/sum(self.prob_m) for x in kio]
+        self.prob_w = nio
 
         fig = plt.figure(figsize=(12, 6))  # create a figure
-        
-        plt.plot(self.w_values, self.normalised_prob, 'o-')
-        
+        plt.plot(self.w_values, self.prob_w, 'o-')    
         plt.xlabel('W Values')
-        plt.ylabel('Normalised Probability')
-        
+        plt.ylabel('Unnormalised Probability')
         plt.show()
  
       
--- Signalgenerator.py
+++ Signalgenerator.py
@@ -5,71 +5,123 @@ import matplotlib.pyplot as plt
 
 
 class SignalGenerator:
+    '''
+    Genetate fake periodic signal
+    Parameters of signal to be determined in dictionary when class is instantiated.
+    Diffrent types of periodic signal are methods of class.
+    Generate pure class is special methods which plots data without sparcity and noise added for visualisation.
+
+    Generated data is of the shape of numpy array,
+    with three columns (time, flux, noise)
+
+    If one methods is followed by another previous generated signal will be overwritten
+
+
+    '''
     def __init__(self, data_generator):
         self.data_generator = data_generator
         self.data = None
         self.flux = None
+        self.time = np.linspace(0, self.data_generator['time_range'], self.data_generator['no_data_points'])#time with asked resolution
+        self.time_p = np.linspace(0, self.data_generator['time_range'], 1000)#high resolution time for ploting only
+        '''
+        fill entries of folling dictionary before instantiating:
+
+        Data_generator = {
+                          'time_range': , 'no_data_points': , 'sparcity': , 
+                          'frequency': , 'Phase': , 'Amplitude': , 'DC':  ,
+                          'Noise': , 'Random_seed': , 'asymetry' :    
+                        }
+
+        '''
 
     def generate_sine_signal(self):
-        np.random.seed(self.data_generator['Random_seed'])
-        time = np.linspace(1, self.data_generator['time_range'], self.data_generator['no_data_points'])
-        time_p = np.linspace(1, self.data_generator['time_range'], 1000)
-        amp = self.data_generator['Amplitude']
-        flux = amp*np.sin(time * self.data_generator['frequency'] + self.data_generator['Phase']) + self.data_generator['DC']
-        flux_p = amp*np.sin(time_p * self.data_generator['frequency'] + self.data_generator['Phase']) + self.data_generator['DC']
-        self.flux = flux_p
+        #generate one pure signal and one noisy signal
+
+        np.random.seed(self.data_generator['Random_seed']) #use random seed
+
+        amp = self.data_generator['Amplitude'] #use amplitude specefied
+
+        flux = amp*np.sin(2*np.pi*self.time * self.data_generator['frequency'] + self.data_generator['Phase']) + self.data_generator['DC']
+        flux_p = amp*np.sin(2*np.pi*self.time_p * self.data_generator['frequency'] + self.data_generator['Phase']) + self.data_generator['DC']
+        #Requested flux and flux_p for ploting noisy free
+
+        self.flux = flux_p # flux requested
+        # add noise to flux
         noise = np.random.normal(0, self.data_generator['Noise'], self.data_generator['no_data_points'])
         flux_noisy = flux + noise
-        df = pd.DataFrame({'time': time, 'flux': flux_noisy, 'error': self.data_generator['Noise']})
+
+        # make data frame requested
+        df = pd.DataFrame({'time': self.time, 'flux': flux_noisy, 'error': self.data_generator['Noise']})
+
+        # make data-set sparse
         num_rows_to_delete = int(len(df) * self.data_generator['sparcity'] / 100)
         indices_to_delete = np.random.choice(df.index, size=num_rows_to_delete, replace=False)
         df = df.drop(indices_to_delete)
+
+        #convert to numpy array
         self.data = np.column_stack((df['time'], df['flux'], df['error']))
         return self.data
 
+
+
     def generate_triangular_signal(self):
-        np.random.seed(self.data_generator['Random_seed'])
-        time = np.linspace(1, self.data_generator['time_range'], self.data_generator['no_data_points'])
-        time_p = np.linspace(1, self.data_generator['time_range'], 1000)
-        amp = self.data_generator['Amplitude']
+        #generate one pure signal and one noisy signal
+
+        np.random.seed(self.data_generator['Random_seed'])#use random seed
+
+        amp = self.data_generator['Amplitude']#use amplitude specefied
+
         flux = (
-            amp*sawtooth(time * self.data_generator['frequency'] + self.data_generator['Phase'],
+                     amp*sawtooth(self.time * self.data_generator['frequency'] + self.data_generator['Phase'],
                      self.data_generator['asymetry']) +
-            self.data_generator['DC']
+                     self.data_generator['DC']
         )
         flux_p = (
-            amp*sawtooth(time_p * self.data_generator['frequency'] + self.data_generator['Phase'],
+            amp*sawtooth(self.time_p * self.data_generator['frequency'] + self.data_generator['Phase'],
                      self.data_generator['asymetry']) +
             self.data_generator['DC']
-        )
-        self.flux = flux_p
+        )#Requested flux and flux_p for ploting noisy free
+
+        self.flux = flux_p # flux requested
+        # add noise to flux
         noise = np.random.normal(0, self.data_generator['Noise'], self.data_generator['no_data_points'])
         flux_noisy = flux + noise
-        df = pd.DataFrame({'time': time, 'flux': flux_noisy, 'error': self.data_generator['Noise']})
+
+        # make data frame requested
+        df = pd.DataFrame({'time': self.time, 'flux': flux_noisy, 'error': self.data_generator['Noise']})
+
+        # make data-set sparse        
         num_rows_to_delete = int(len(df) * self.data_generator['sparcity'] / 100)
         indices_to_delete = np.random.choice(df.index, size=num_rows_to_delete, replace=False)
         df = df.drop(indices_to_delete)
+
+        #convert to numpy array      
         self.data = np.column_stack((df['time'], df['flux'], df['error']))
         return self.data
 
     def plot_pure_data(self):
+        #plot pure data, noise free, uniformed sampled
         if self.data is None:
             print("No data available. Generate a signal first.")
             return
-        time_p = np.linspace(1, self.data_generator['time_range'], 1000)
+        #time_p = np.linspace(0, self.data_generator['time_range'], 1000)
         flux = self.flux
-        plt.plot(time_p, flux)
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(self.time_p, self.flux)
         plt.xlabel('Time')
         plt.ylabel('Flux')
         plt.title('Time Series Data')
         plt.show()
         
     def plot_noise_data(self):
+        #Plot requested data
         if self.data is None:
             print("No data available. Generate a signal first.")
             return
         time = self.data[:, 0]
         flux = self.data[:, 1]
+        fig = plt.figure(figsize=(12, 6))
         plt.plot(time, flux)
         plt.xlabel('Time')
         plt.ylabel('Flux')
